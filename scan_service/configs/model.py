a82456be781776ccd2bfc463c72eda234b65b878
vodaiphuoc <phuocvodn98@gmail.com>
2025-07-21 18:29:01 +0700

    test

--- scan_service/configs/model.py
+++ scan_service/configs/model.py
@@ -26,7 +26,7 @@ class GenerationConfig(BaseModel):
 
 class ModelConfig(BaseModel):
     model_id:str = Field(
-        default="OpenGVLab/InternVL3-8B-Instruct",
+        default="OpenGVLab/InternVL3-1B-Instruct",
         # default="OpenGVLab/InternVL3-1B",
         # default = "5CD-AI/Vintern-1B-v3_5",
         description="repo id on huggingface"
