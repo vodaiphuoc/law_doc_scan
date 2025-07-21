22f5310817457b57f102b02d2bbdd3792e952d7a
vodaiphuoc <phuocvodn98@gmail.com>
2025-07-21 19:22:00 +0700

    test

--- scan_service/configs/model.py
+++ scan_service/configs/model.py
@@ -21,7 +21,7 @@ class GenerationConfig(BaseModel):
     # top_k: int = 64
     # top_p: float = 0.95
     max_new_tokens: int = 1024 
-    do_sample: bool  = True
+    do_sample: bool  = False
     repetition_penalty:float = 2.0
 
 class ModelConfig(BaseModel):
