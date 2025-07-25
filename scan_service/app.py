8007769ec82e7fe58dee1123dd3b22cf41d56a5b
vodaiphuoc <phuocvodn98@gmail.com>
2025-07-25 20:11:43 +0700

    todo: push result after processing

--- scan_service/app.py
+++ scan_service/app.py
@@ -1,153 +1,133 @@
-# from uuid import uuid4
-# from fastapi import UploadFile, Request, FastAPI
-# from fastapi.responses import JSONResponse
-# import uvicorn
-# import os
-# from typing import List, Dict, Union, Any
-# from types import NoneType
-# from contextlib import asynccontextmanager
-# from dotenv import load_dotenv
-# import asyncio
-# import json
-# from aio_pika.abc import AbstractIncomingMessage
-
-# # from .components import pdf2imgs, UnslothExtractModel
-# from commons import (
-#     QueueConsumer, 
-#     make_logger,
-#     ExtractModelResult,
-#     Firebase_Client,
-#     MQ_SETTINGS,
-#     GENERAL_SETTINGS
-# )
-
-# SERVICE_NAME = "Extract service"
-# ServiceLogger = make_logger(SERVICE_NAME)
-# TEMP_DATA_PATH = __file__.replace("app.py",".temp_data")
-
-# class ModelInference(object):
-#     def __init__(self):
-#         self.engine = UnslothExtractModel()
-#         self.pdf2imgs = pdf2imgs
-    
-#     def run(self, file_path: str)->Dict[str, Union[str, bool, NoneType, Dict[str, Any]]]:
-#         api_response_data = None
-
-#         img_paths = self.pdf2imgs(file_path, resize= False)
-
-#         _results = self.engine.forward(img_paths = img_paths)
-#         print('_results: ', _results)
-
-#         if _results['status']:
-#             api_response_data = {
-#                 "status": _results['status'],
-#                 "msg": _results['msg'],
-#                 "result": _results['result'].model_dump_json(indent = 2)
-#             }
-#         else:
-#             api_response_data = {
-#                 "status": _results['status'],
-#                 "msg": _results['msg'],
-#                 "result": None
-#             }
-
-#         # empty temporary data
-#         os.remove(file_path)
-#         for _img_path in img_paths:
-#             os.remove(_img_path)
-    
-#         return api_response_data
-
-# async def message_handler(
-#         message: AbstractIncomingMessage,
-#         model_instance: ModelInference,
-#         storage_client: Firebase_Client
-#     ) -> None:
-
-#     # extract data
-#     async with message.process():
-#         body_data = json.loads(message.body.decode())
-#         ServiceLogger.info('receive data from queue: {msg}'.format(msg = body_data))
-
-#         # parsing data
-#         _temp_local_file = f'{TEMP_DATA_PATH}/temp_write_{"_".join(body_data["blob_name"].split(os.sep))}'
-#         storage_client.download_blob(**body_data, destination_file_name = _temp_local_file)
-
-
-#         extract_result = model_instance.run(_temp_local_file)
-        
-
-# @asynccontextmanager
-# async def lifespan(app: FastAPI):
-#     load_dotenv()
-#     loop = asyncio.get_event_loop()
-#     model_engine = ModelInference()
-#     storage_client = Firebase_Client(service_name= SERVICE_NAME)
-
-#     queue_client = QueueConsumer(
-#         user = MQ_SETTINGS.USER, 
-#         pwd = MQ_SETTINGS.PWD, 
-#         host_name = MQ_SETTINGS.HOSTNAME, 
-#         port = MQ_SETTINGS.PORT,
-#         queue_name = MQ_SETTINGS.RANKING_SERVICE_QUEUE_NAME,
-#         call_back = message_handler,
-#         service_logger = ServiceLogger,
-#         model_instance = model_engine,
-#         storage_client = storage_client
-#     )
-
-#     queue_client.start_background_task(loop)
-
-#     ServiceLogger.info('app done init')
-#     yield
-#     ServiceLogger.info('app shutting down. Cancelling background task...')
-#     await queue_client.stop_background_task()
-#     ServiceLogger.info('app shutdown complete.')
-
-
-# main_app = FastAPI(lifespan= lifespan, root_path="extract_service")
-
-# @main_app.get("/")
-# async def index():
-#     return JSONResponse("hello word from extract service")
-
-# async def main():
-#     config = uvicorn.Config("app:app",
-#                             host = GENERAL_SETTINGS.APPLICATION_HOST,
-#                             port = GENERAL_SETTINGS.EXTRACT_SERVICE_PORT,
-#                             reload = True,
-#                             log_level = "info"
-#                             )
-#     server = uvicorn.Server(config)
-#     await server.serve()
-
-# if __name__ == "__main__":
-#     asyncio.run(main())
-
-
 import sys
 import os
-import argparse
 import asyncio
+import tempfile
+
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+import uvicorn
+from contextlib import asynccontextmanager
+
+from dotenv import load_dotenv
+from aio_pika.abc import AbstractIncomingMessage
+
 
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '../'))
 if project_root not in sys.path:
     sys.path.append(project_root)
 
-from model.infer import ModelWrapperClient
+
+from .model_client import ModelWrapperClient
 from commons.configs.model import ModelConfigQuant
+from commons.logger import get_logger
+from commons.configs.apis import (
+    MQ_SETTINGS,
+    GENERAL_SETTINGS
+)
+from commons.schemas.mq_messages import ScanRequest
+from commons.queue_modeling import QueueConsumer
+from commons.database_storage import DB_Storage_Client
 
-engine = ModelWrapperClient(config= ModelConfigQuant())
+SERVICE_NAME = "Extract service"
+ServiceLogger = get_logger(SERVICE_NAME)
+TEMP_DATA_PATH = __file__.replace("app.py",".temp_data")
 
-async def main(args: argparse.Namespace):
 
-    await engine.forward(local_path_pdf = args.file_path, task = args.task)
+async def message_handler(
+        message: AbstractIncomingMessage,
+        engine: ModelWrapperClient,
+        storage_client: DB_Storage_Client
+    ) -> None:
+    r"""
+    Callback function for handling scan file request
+    """
+    
+    async with message.process():
+        # extract data
+        msg: str = message.body.decode()
+        req_data: ScanRequest = ScanRequest.model_validate_json(msg)
+
+        ServiceLogger.info('receive data from queue: {msg}'.format(msg = msg))
+
+        # process with temporary file
+        with tempfile.NamedTemporaryFile(
+                suffix=".pdf",
+                mode='wb', 
+                delete=True, 
+            ) as temp_pdf_file:
+
+            file_content = storage_client.download_pdf(
+                bucket_name=req_data.remote_path, 
+                remote_path=req_data.remote_path
+            )
+
+            temp_pdf_file.write(file_content)
+
+            # Ensure all data is written to disk before accessing via its name
+            temp_pdf_file.flush()
+            os.fsync(temp_pdf_file.fileno())
+
+            # Get the path to the temporary PDF file
+            temp_pdf_path = temp_pdf_file.name
+            
+            extract_result = await engine.forward(
+                local_path_pdf= temp_pdf_path,
+                task = req_data.task
+            )
+
+        if extract_result is None:
+            ServiceLogger.info('failed to extract data')
+        else:
+            ...
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    load_dotenv()
+    loop = asyncio.get_event_loop()
+    # model_engine = ModelInference()
+
+    engine = ModelWrapperClient(config= ModelConfigQuant())
+
+    storage_client = DB_Storage_Client()
+
+    queue_client = QueueConsumer(
+        user = MQ_SETTINGS.USER,
+        pwd = MQ_SETTINGS.PWD, 
+        host_name = MQ_SETTINGS.HOSTNAME, 
+        port = MQ_SETTINGS.PORT,
+        queue_name = MQ_SETTINGS.RANKING_SERVICE_QUEUE_NAME,
+        call_back = message_handler,
+        service_logger = ServiceLogger,
+        engine = engine,
+        storage_client = storage_client
+    )
+
+    queue_client.start_background_task(loop)
+
+    ServiceLogger.info('app done init')
+    yield
+    ServiceLogger.info('app shutting down. Cancelling background task...')
+    await queue_client.stop_background_task()
+    ServiceLogger.info('app shutdown complete.')
+
+
+main_app = FastAPI(lifespan= lifespan)
+
+@main_app.get("/")
+async def index():
+    return JSONResponse("hello word from scan service")
+
+async def main():
+    config = uvicorn.Config("app:app",
+                            host = GENERAL_SETTINGS.APPLICATION_HOST,
+                            port = GENERAL_SETTINGS.EXTRACT_SERVICE_PORT,
+                            reload = True,
+                            log_level = "info"
+                            )
+    server = uvicorn.Server(config)
+    await server.serve()
 
 if __name__ == "__main__":
-    
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--file_path", type=str)
-    parser.add_argument("--task",type=str)
-    args = parser.parse_args()
-    
-    asyncio.run(main(args))
+    asyncio.run(main())
+
